7c4ac60e2d4b8a1c74d6bed05f7d415fe0e3836b
Carolina Reis <carolreis0116@gmail.com>
2022-12-02 20:52:33 +0000

    jogo revisto e comentado

--- snake.py
+++ snake.py
@@ -9,8 +9,9 @@ DEFAULT_SIZE = 20
 SNAKE_SHAPE = 'square'
 HIGH_SCORES_FILE_PATH = 'high_scores.txt'
 # Controla a velocidade da cobra. Quanto menor o valor, mais rápido é o movimento da cobra.
-speed = 0.25
+speed = 0.25  # Constante alterada para variável (útil para a construção dos diferentes níveis de jogo)
 turtle.title("Happy Snack Game")
+# Lista de imagens que vão ser adicionadas como shapes (mais tarde)
 foods = ['banana.gif', 'bread.gif', 'candy.gif', 'chocolate.gif', 'cookie.gif',
          'donut.gif', 'doritos.gif', 'egg.gif', 'pizza.gif', 'popcorn.gif']
 
@@ -21,15 +22,11 @@ def load_high_score(state):
         state['high_score'] = state['score']
     return state['high_score']
 
-
 def write_high_score_to_file(state):
     # devem escrever o valor que está em state['high_score'] no ficheiro de high scores
-    f = open(HIGH_SCORES_FILE_PATH, 'w')
-    f.seek(0)
-    f.truncate()
-    f.write(str(load_high_score(state)))
-    f.close()
-
+    file = open(HIGH_SCORES_FILE_PATH, 'a')  # Modo 'a' append: adiciona o high score à última linha do ficheiro
+    file.write('\n' + str(load_high_score(state)))
+    file.close()
 
 def create_score_board(state):
     score_board = turtle.Turtle()
@@ -38,62 +35,56 @@ def create_score_board(state):
     score_board.color('white')
     score_board.penup()
     score_board.hideturtle()
-    score_board.goto(0, MAX_Y / 2.3)
+    score_board.goto(0, MAX_Y / 2.4)  # Valor 2,2 alterado para 2,4 (por questões meramente visuais)
     state['score_board'] = score_board
     load_high_score(state)
     update_score_board(state)
 
-
 def update_score_board(state):
-    file = open(HIGH_SCORES_FILE_PATH, "r")
+    file = open(HIGH_SCORES_FILE_PATH, "r")  # Modo 'r' read: lê o ficheiro dos high scores (neste caso, a última linha)
     state['score_board'].clear()
-    state['score_board'].write("Score: {} High Score: {}".format(state['score'], file.readline()), align="center",
+    state['score_board'].write("Score: {} High Score: {}".format(state['score'], file.readlines()[-1]), align="center",
                                font=("Helvetica", 24, "normal"))
     file.close()
-
 
 def go_up(state):
     if state['snake']['current_direction'] != 'down':
         state['snake']['current_direction'] = 'up'
 
-
 def go_down(state):
     if state['snake']['current_direction'] != 'up':
         state['snake']['current_direction'] = 'down'
-
 
 def go_left(state):
     if state['snake']['current_direction'] != 'right':
         state['snake']['current_direction'] = 'left'
 
-
 def go_right(state):
     if state['snake']['current_direction'] != 'left':
         state['snake']['current_direction'] = 'right'
 
-
 def init_state():
     segments = []  # Lista que vai armazenar os segmentos do corpo
-    file = open(HIGH_SCORES_FILE_PATH, "r")
-
-    state = {'score_board': None, 'new_high_score': False, 'high_score': int(file.readline()), 'score': 0, 'food': None, 'window': None,
-             'body': segments, 'level': turtle.Turtle()}
-    file.close()
+    file = open(HIGH_SCORES_FILE_PATH, "r")  # Modo 'r' read: lê o ficheiro dos high scores (neste caso, a última linha)
     # Informação necessária para a criação do score board
     # Para gerar a comida deverá criar uma nova tartaruga e colocar a mesma numa posição aleatória do campo
+    state = {'score_board': None, 'high_score': int(file.readlines()[-1]), 'score': 0, 'food': None, 'window': None,
+             'body': segments, 'level': turtle.Turtle()}
     snake = {
         'head': None,  # Variável que corresponde à cabeça da cobra
         'current_direction': None  # Indicação da direção atual do movimento da cobra
     }
     state['snake'] = snake
+    file.close()
     return state
 
 
 def setup(state):
     window = turtle.Screen()
-    window.setup(width=MAX_X, height=MAX_Y, starty=0)
-    window.bgcolor(27 / 255, 94 / 255, 32 / 255)
-    window.bgpic('snake_600_800.png')
+    window.setup(width=MAX_X, height=MAX_Y, starty=0)  # Ajuste na posição (y) de inicialização do ecrã
+    window.bgcolor(27 / 255, 94 / 255, 32 / 255)  # Cor de fundo
+    window.bgpic('snake_600_800.png')  # Imagem de fundo
+    # Adição de imagens como shapes
     window.addshape('block_body.gif')
     window.addshape('level.gif')
     for i in foods:
@@ -107,36 +98,32 @@ def setup(state):
         window.onkey(functools.partial(go_right, state), keyboard[i + 3])
     window.tracer(0)
     state['window'] = window
-
     snake = state['snake']
     snake['current_direction'] = 'stop'
     snake['head'] = turtle.Turtle()
     snake['head'].shape(SNAKE_SHAPE)
     snake['head'].pu()
-    snake['head'].color('green')
-
+    snake['head'].color(27 / 255, 94 / 255, 32 / 255)  # Alteração da cor da cabeça da snake, para coincidir com a cor de fundo do ecrã
     create_score_board(state)
     create_food(state)
-
 
 def move(state):
     """
     Função responsável pelo movimento da cobra no ambiente.
     """
     snake = state['snake']
-    if state['snake']['current_direction'] == 'up':
+    if state['snake']['current_direction'] == 'up':  # Movimento para cima
         snake['head'].setheading(90)
         snake['head'].fd(20)
-    if state['snake']['current_direction'] == 'left':
+    if state['snake']['current_direction'] == 'left':  # Movimento para a esquerda
         snake['head'].setheading(180)
         snake['head'].fd(20)
-    if state['snake']['current_direction'] == 'right':
+    if state['snake']['current_direction'] == 'right':  # Movimento para a direita
         snake['head'].setheading(0)
         snake['head'].fd(20)
-    if state['snake']['current_direction'] == 'down':
+    if state['snake']['current_direction'] == 'down':  # Movimento para baixo
         snake['head'].setheading(-90)
         snake['head'].fd(20)
-
 
 def create_food(state):
     """
@@ -144,70 +131,65 @@ def create_food(state):
     mas dentro dos limites do ambiente.
     """
     # a informação sobre a comida deve ser guardada em state['food']
-
-    x = random.randint(-13, 13) * 20
-    y = random.randint(-18, 17) * 20
-
+    # Randomiza-se um número e multiplica-se por 20, para assegurar a colocação da comida numa espécie de grelha,
+    # dado que a snake anda sempre de 20 em 20 unidades (para qualquer direção)
+    x = random.randint(-13, 13) * 20  # 13 * 20 = (600 / 2) - (20 * 2) = 260
+    y = random.randint(-18, 16) * 20  # 18 * 20 = (800 / 2) - (20 * 2) = 360
+    # NOTA: no y, é usado 16 como valor máximo, para prevenir a geração de comida em cima da score board
     state['food'] = turtle.Turtle()
-    state['food'].shape(random.choice(foods))
+    state['food'].shape(random.choice(foods))  # A shape da comida vai ser aleatoriamente escolhida, através da lista inicial
     state['food'].pu()
-    state['food'].goto(x, y)
-    state['food'].color('red')
-
-    check_food_body_collisions(state)
-    return state['food']
-
+    state['food'].setposition(x, y)
+    check_food_body_collisions(state)  # Função criada para verificar se a comida é gerada dentro do corpo da snake
 
 def check_if_food_to_eat(state):
     """
     Função responsável por verificar se a cobra tem uma peça de comida para comer. Deverá considerar que se a comida
     estiver a uma distância inferior a 15 píxeis a cobra pode comer a peça de comida.
     """
-    global speed
+    global speed  # Variável global de speed de jogo vai alterar conforme o score do jogador
     food = state['food']
     snake = state['snake']
     segments = state['body']
+    # Se a cabeça da snake está a uma distância inferior a 15 píxeis da comida
     if snake['head'].distance(food) < 15:
         food.hideturtle()  # Esconde a comida "apanhada"
         create_food(state)  # Gera uma nova comida
-
         # Adição de um novo segmento corporal à lista "segments"
         body_segment = turtle.Turtle()
         body_segment.shape('block_body.gif')
         body_segment.pu()
         body_segment.color('black')
         segments.append(body_segment)
-
+        # Incrementa-se o score em 10 pontos
         state['score'] += 10
+        # Se o score for um múltiplo de 200 e estiver entre 0 e 1000 (inclusive),
+        # a velocidade vai aumentar (nível seguinte)
         if state['score'] % 200 == 0 and 0 < state['score'] <= 1000:
-            if speed > 0.05:
+            if speed > 0.05:  # Verificação, para evitar atribuição de velocidades negativas
                 speed -= 0.04
         write_high_score_to_file(state)
-    # para ler ou escrever os valores de high score, score e new high score, devem usar os respetivos campos do
-    # state: state['high_score'], state['score'] e state['new_high_score']
-
 
 def boundaries_collision(state):
     """
     Função responsável por verificar se a cobra colidiu com os limites do ambiente. Sempre que isto acontecer a
     função deverá returnar o valor booleano True, caso contrário retorna False.
     """
-    global speed
+    global speed  # Variável global de speed de jogo vai ser resetada para o seu valor inicial
     snake = state['snake']
     food = state['food']
     segments = state['body']
-
-    # Se a cabeça da cobra ultrapassou os limites do ambiente...
+    # Se a cabeça da cobra ultrapassou os limites do ambiente (600 / 2 - 20 = 280 e 800 / 2 - 20 = 380)...
     if snake['head'].xcor() > 280 or snake['head'].xcor() < -280 or snake['head'].ycor() > 380 or snake['head'].ycor() < -380:
+        # "Escondem-se" as turtles da snake, a comida e a turtle de passagem de nível
         snake['head'].ht()
-        food.hideturtle()
         for i in segments:
             i.hideturtle()
+        food.hideturtle()
         state['level'].ht()
         speed = 0.25
         return True
     return False
-
 
 def check_collisions(state):
     """
@@ -215,56 +197,60 @@ def check_collisions(state):
     os limites do ambiente. No entanto, deverá escrever o código para verificar quando é que a tartaruga choca com uma
     parede ou com o seu corpo.
     """
-    global speed
+    global speed  # Variável global de speed de jogo vai ser resetada para o seu valor inicial
     snake = state['snake']
     food = state['food']
     segments = state['body']
-
+    # Se a cabeça da cobra chocou com alguma das partes do corpo...
     for i in segments:
         if i.distance(snake['head']) < 15:
+            # "Escondem-se" as turtles da snake, a comida e a turtle de passagem de nível
             snake['head'].ht()
-            food.hideturtle()
             for j in segments:
                 j.hideturtle()
+            food.hideturtle()
             state['level'].ht()
             speed = 0.25
             return True
     return boundaries_collision(state)
 
-
-# Função que verifica se a comida foi gerada dentro de alguma das partes do corpo
 def check_food_body_collisions(state):
+    """
+    Função que verifica se a comida foi gerada dentro de alguma das partes do corpo
+    """
     food = state['food']
     segments = state['body']
     for i in segments:
-        if i.distance(food) < 15:  # Em caso disso ocorrer, cria uma comida nova (noutro local)
+        if i.distance(food) < 15:  # Se isso ocorrer, cria uma comida nova (noutro local)
             food.hideturtle()
             create_food(state)
     if food.distance(state['level']) < 25:  # EXTRA (colisões com o ícone de level up)
         food.hideturtle()
         create_food(state)
 
-# Aumenta a velocidade do jogo consoante a pontuação (existem 5 níveis possíveis)
 def level_up(state):
+    """
+    Informa da passagem de nível (com imagem)
+    NOTA: existem 5 níveis de jogo
+    """
     state['level'].pu()
     state['level'].setposition(-150, -200)
     state['level'].shape('level.gif')
     state['level'].ht()
-
+    # Se o score for um múltiplo de 200 e estiver entre 0 e 1000 (inclusive),
+    # a imagem de level up vai aparecer
     if state['score'] % 200 == 0 and 0 < state['score'] <= 1000:
         state['level'].st()
     else:
         state['level'].ht()
 
-
 def main():
-    while True:
+    while True:  # Quando o jogador perde, o jogo recomeça automaticamente
         state = init_state()
         setup(state)
         snake = state['snake']
         segments = state['body']
-
-        while not check_collisions(state):
+        while not check_collisions(state):  # Enquanto não houverem colisões...
             state['window'].update()
             check_if_food_to_eat(state)
             update_score_board(state)
@@ -287,17 +273,17 @@ def main():
 
 main()
 
-# Nome: Luana Carolina Cunha Reis
-# Nº de estudante: 2022220606
+"""
+Nome: Luana Carolina Cunha Reis
+Nº de estudante: 2022220606
 
 # Nome: Diogo Ramos Barbosa
 # Nº de estudante: 2021234034
+"""
 
 '''
-
 Referências:
 Fundo do jogo: https://wallpapercave.com/cartoon-snake-wallpapers
 Corpo da snake: https://github.com/codebasics/python_projects/blob/main/1_snake_game/resources/block.jpg
 Templates das comidas: https://www.canva.com/
-
 '''
